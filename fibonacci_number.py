d4de8ff96f052ffc5bd67b916270768200ba4c1e
11054 <71435413+11054@users.noreply.github.com>
2020-10-06 12:28:13 +0200

    Add files via upload

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -1,31 +1,22 @@
 
-scope = int(input("insert some number: ")) # 90
-
+scope = int(input("insert some number: "))  # 90
 
 a = 0
 b = 1
 fib_number = 0
 sum = 0
+89
 
 while fib_number < scope:
-    
+
     fib_number = a + b
     if fib_number > scope:
         break
     a = b
     b = fib_number
     print(fib_number)
-   
+
     if fib_number % 2 == 0:
         sum += fib_number
 
 print("sum of the even-valued terms is:", sum)
-
-    
-        
-
-
-
-        
-
-
